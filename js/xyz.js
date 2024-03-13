e23543dcf876fe0a0a787cf781b3d9f1b129f7e3
Panga-Deepthi <pangadeepthi012@gmail.com>
2024-03-13 17:20:59 +0530

    IMS

---
+++ js/xyz.js
@@ -0,0 +1,83 @@
+$(document).ready(function(){
+    console.log("success");
+	var DOMAIN = "http://localhost/ims1/ims";
+	$("#register_form").on("submit",function(){
+		var status = false;
+		var name = $("#username");
+		var email = $("#email");
+		var pass1 = $("#password1");
+		var pass2 = $("#password2");
+		var type = $("#usertype");
+		//rizwan@gmail.com
+		var e_patt = new RegExp(/^[a-z0-9_-]+(\.[a-z0-9_-]+)*@[a-z0-9_-]+(\.[a-z0-9_-]+)*(\.[a-z]{2,4})$/);
+		if(name.val() == "" || name.val().length < 6){
+			name.addClass("border-danger");
+			$("#u_error").html("<span class='text-danger'>Please Enter Name and Name should be more than 6 char</span>");
+			status = false;
+		}else{
+			name.removeClass("border-danger");
+			$("#u_error").html("");
+			status = true;
+		}
+		if(!e_patt.test(email.val())){
+			email.addClass("border-danger");
+			$("#e_error").html("<span class='text-danger'>Please Enter Valid Email Address</span>");
+			status = false;
+		}else{
+			email.removeClass("border-danger");
+			$("#e_error").html("");
+			status = true;
+		}
+		if(pass1.val() == "" || pass1.val().length < 9){
+			pass1.addClass("border-danger");
+			$("#p1_error").html("<span class='text-danger'>Please Enter more than 9 digit password</span>");
+			status = false;
+        }else{
+			pass1.removeClass("border-danger");
+			$("#p1_error").html("");
+			status = true;
+		}
+		if(pass2.val() == "" || pass2.val().length < 9){
+			pass2.addClass("border-danger");
+			$("#p2_error").html("<span class='text-danger'>Please Enter more than 9 digit password</span>");
+			status = false;
+		}else{
+			pass2.removeClass("border-danger");
+			$("#p2_error").html("");
+			status = true;
+		}
+		if(type.val() == ""){
+			type.addClass("border-danger");
+			$("#t_error").html("<span class='text-danger'>Please Enter more than 9 digit password</span>");
+			status = false;
+		}else{
+			type.removeClass("border-danger");
+			$("#t_error").html("");
+			status = true;
+		}
+        if ((pass1.val() == pass2.val()) && status == true) {
+			$(".overlay").show();
+			$.ajax({
+				url : DOMAIN+"/register.php",
+				method : "POST",
+				data : $("#register_form").serialize(),
+				success : function(data){
+					if (data == "EMAIL_ALREADY_EXISTS") {
+						$(".overlay").hide();
+						alert("It seems like you email is already used");
+					}else if(data == "SOME_ERROR"){
+						$(".overlay").hide();
+						alert("Something Wrong");
+					}else{
+						$(".overlay").hide();
+						window.location.href = encodeURI(DOMAIN+"/login.php?msg=You are registered Now you can login");
+					}
+				}
+			})
+		}else{
+			pass2.addClass("border-danger");
+			$("#p2_error").html("<span class='text-danger'>Password is not matched</span>");
+			status = true;
+		}
+	})
+})
